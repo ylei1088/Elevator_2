cae1d3e6034b82bf0db984353f47c6231664b934
Yongxin_Lei Lei <Yongxin.Lei@iam.local>
2020-03-07 12:22:35 -0600

    Elevetor 2 done!

--- main.c
+++ main.c
@@ -3,22 +3,69 @@
 
 int main(){
 
-    int floors [16] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-    int size = sizeof(floors)/sizeof(floors[0]);
+    int Floors [16] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    int sizeFloors = sizeof(Floors)/sizeof(Floors[0]);
 
-    int currentFloor;
+    int chosenFloors[5];
+    int sizeChosenFloors = sizeof(chosenFloors)/sizeof(chosenFloors[0]);
+
+    int currentFloor = 0;
     int playerFloor;
+    int checkValidFloor = 1;
 
+    
 
-    printf("Choose a floor: ");
-    scanf("\n%d", &playerFloor);
+    for(int i = 0; i < sizeChosenFloors; i++)
+    {   
+        printf("Choose a floor: ");
+        scanf("\n%d", &playerFloor);
 
-    for(int i = 0; i < size; i++)
-    {
-        if(playerFloor >= i)
+        for(int j = 0; j < sizeFloors; j++)
         {
-            printf(i); 
+            if(playerFloor == Floors[j])
+            {
+                chosenFloors[i] = playerFloor;
+            }
         }
+    }
+
+    if(checkValidFloor == 1)
+    {
+        for(int i = 0; i < sizeChosenFloors; i++)
+        {
+            if(chosenFloors[i] < currentFloor)
+            {
+                for(int j = currentFloor; j >= chosenFloors[i]; j--)
+                {
+                    if(chosenFloors[i] <= Floors[j])
+                    {
+                        printf("%d\n",j);
+                    }
+                }
+                
+                printf("DING!\n");
+                printf("Next chosen floor: \n");
+            }
+
+            if(chosenFloors[i] > currentFloor)
+            {
+                for(int j = currentFloor; j < sizeFloors; j++)
+                {
+                    if(chosenFloors[i] >= Floors[j])
+                    {
+                        printf("%d\n",j);
+                    }
+                }
+                printf("DING!\n");
+                printf("Next chosen floor: \n");
+            }
+            
+            currentFloor = chosenFloors[i];
+        }
+    }
+    else
+    {
+        printf("Invalid Floor Inserted !");
     }
 
     return 0;
